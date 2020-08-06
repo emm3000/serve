c81239115c84eb19dbca84438cfe4362f79fe43b
Edgardo Muñoz <edgardo.emm.20@gmail.com>
2020-08-06 17:24:12 -0500

    FINAL+

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const path = require("path")
-const history = require("connect-history-api-fallback")
-
-const PORT = process.env.PORT || 3000
-
-app.use(cors())
-app.use(express.static(path.join(__dirname, "/dist")))
-console.log(path.join(__dirname, "/dist"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const express = require("express");
+const app = express();
+const cors = require("cors");
+// const path = require("path");
+// const history = require("connect-history-api-fallback");
+const mongoose = require("mongoose");
 
 
-app.use(history());
-app.use(express.static(path.join(__dirname, 'dist')));
+function randomNumber () {
+  return Math.round(Math.random() * 100)
+}
 
-app.get("/prueba", (req, res) => {
+mongoose.connect("mongodb://mongo/nginx", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(_ => {
+  console.log("Conectado a mongo")
+});
+
+const Cat = mongoose.model("Cat", { name: String });
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+// app.use(express.static(path.join(__dirname, "/dist")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(history());
+// app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/gato", async (req, res) => {
+
+  const kitty = new Cat({ name: `${randomNumber()}Emm${randomNumber()}` });
+  // const aea = await kitty.save()
+
+  res.json(await kitty.save());
+});
+
+app.get("/ww", (req, res) => {
+
   res.json({
-    hola: "manito"
-  })
-})
+    hola: "aea manitowww"
+  });
+});
 
 // app.get("/*", (req, res) => {
 //   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 // })
 
 app.listen(PORT, () => {
-  console.log(`Escuchando en el puerto ${PORT}`)
-})
+  console.log(`Escuchando en el puerto ${PORT}`);
+});
